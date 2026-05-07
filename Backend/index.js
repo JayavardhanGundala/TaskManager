946f7ac713258e0d4446087ec34fe3e1ce84618d
jayavardhan <jayavardhan9911@gmail.com>
2026-05-08 00:36:10 +0530

    cord error

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,17 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import connectDB from "./config/db.js"
-import authRoutes from './routes/authRoutes.js';
-import projectRoutes from './routes/ProjectRoutes.js';
-import taskRoutes from './routes/taskRoutes.js';
-const app=express()
-dotenv.config()
-connectDB() 
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+import authRoutes from "./routes/authRoutes.js";
+import projectRoutes from "./routes/ProjectRoutes.js";
+import taskRoutes from "./routes/taskRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
 const allowedOrigins = [
   "http://localhost:5173",
   "https://task-manager-lac-six-32.vercel.app"
@@ -18,16 +22,12 @@ app.use(cors({
   credentials: true
 }));
 
-app.options("*", cors());
-
 app.use(express.json());
-
 
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/tasks', taskRoutes);
 
-
-app.listen(process.env.PORT,()=>{
-    console.log("server running",process.env.PORT)
-})
+app.listen(process.env.PORT, () => {
+  console.log("server running", process.env.PORT);
+});
